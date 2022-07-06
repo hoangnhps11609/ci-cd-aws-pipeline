d978718a2da3627639cd1dc80fde965c564ee12c
hoangnhps11609 <hoangnhps11609@gmail.com>
2022-07-06 14:16:32 +0700

    update

--- src/main/java/com/myorg/MyPipelineStack.java
+++ src/main/java/com/myorg/MyPipelineStack.java
@@ -2,16 +2,11 @@ package com.myorg;
 
 import java.util.Arrays;
 import software.constructs.Construct;
-import software.amazon.awscdk.Environment;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
-import software.amazon.awscdk.StageProps;
 import software.amazon.awscdk.pipelines.CodePipeline;
 import software.amazon.awscdk.pipelines.CodePipelineSource;
 import software.amazon.awscdk.pipelines.ShellStep;
-
-// import software.amazon.awscdk.Duration;
-// import software.amazon.awscdk.services.sqs.Queue;
 
 public class MyPipelineStack extends Stack {
     public MyPipelineStack(final Construct scope, final String id) {
@@ -21,26 +16,12 @@ public class MyPipelineStack extends Stack {
     public MyPipelineStack(final Construct scope, final String id, final StackProps props) {
         super(scope, id, props);
 
-        // The code that defines your stack goes here
-
-        // example resource
-        // final Queue queue = Queue.Builder.create(this, "MyPipelineQueue")
-        //         .visibilityTimeout(Duration.seconds(300))
-        //         .build();
-
-        final CodePipeline pipeline = CodePipeline.Builder.create(this, "pipeline")
-            .pipelineName("MyPipeline")
-            .synth(ShellStep.Builder.create("Synth")
+        CodePipeline pipeline = CodePipeline.Builder.create(this, "pipeline")
+             .pipelineName("MyPipeline")
+             .synth(ShellStep.Builder.create("Synth")
                 .input(CodePipelineSource.gitHub("hoangnhps11609/ci-cd-aws-pipeline", "main"))
                 .commands(Arrays.asList("npm install -g aws-cdk", "cdk synth"))
                 .build())
-            .build();
-        
-        pipeline.addStage(new MyPipelineAppStage(this, "test", StageProps.builder()
-            .env(Environment.builder()
-                .account("030245542280")
-                .region("ap-southeast-1")
-                .build())
-            .build()));
+             .build();
     }
 }
