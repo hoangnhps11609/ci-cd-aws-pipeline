f8c8bc98cf95db5385717af424e4047bd30641ff
hoangnhps11609 <hoangnhps11609@gmail.com>
2022-07-07 15:45:16 +0700

    test application stages

--- src/main/java/com/myorg/MyPipelineStack.java
+++ src/main/java/com/myorg/MyPipelineStack.java
@@ -2,8 +2,10 @@ package com.myorg;
 
 import java.util.Arrays;
 import software.constructs.Construct;
+import software.amazon.awscdk.Environment;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
+import software.amazon.awscdk.StageProps;
 import software.amazon.awscdk.pipelines.CodePipeline;
 import software.amazon.awscdk.pipelines.CodePipelineSource;
 import software.amazon.awscdk.pipelines.ShellStep;
@@ -16,12 +18,19 @@ public class MyPipelineStack extends Stack {
     public MyPipelineStack(final Construct scope, final String id, final StackProps props) {
         super(scope, id, props);
 
-        CodePipeline pipeline = CodePipeline.Builder.create(this, "pipeline")
-             .pipelineName("MyPipeline")
-             .synth(ShellStep.Builder.create("Synth")
+        final CodePipeline pipeline = CodePipeline.Builder.create(this, "pipeline")
+            .pipelineName("MyPipeline")
+            .synth(ShellStep.Builder.create("Synth")
                 .input(CodePipelineSource.gitHub("hoangnhps11609/ci-cd-aws-pipeline", "main"))
                 .commands(Arrays.asList("npm install -g aws-cdk", "cdk synth"))
                 .build())
-             .build();
+            .build();
+        
+        pipeline.addStage(new MyPipelineAppStage(this, "test", StageProps.builder()
+            .env(Environment.builder()
+                .account("030245542280")
+                .region("us-east-1")
+                .build())
+            .build()));
     }
 }
