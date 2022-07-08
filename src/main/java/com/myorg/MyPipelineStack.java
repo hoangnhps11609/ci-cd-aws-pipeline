6b5d50ba68b642823d142c2d23c1f5900f27bfa8
hoangnhps11609 <hoangnhps11609@gmail.com>
2022-07-08 11:10:18 +0700

    testing deployments

--- src/main/java/com/myorg/MyPipelineStack.java
+++ src/main/java/com/myorg/MyPipelineStack.java
@@ -9,6 +9,8 @@ import software.amazon.awscdk.StageProps;
 import software.amazon.awscdk.pipelines.CodePipeline;
 import software.amazon.awscdk.pipelines.CodePipelineSource;
 import software.amazon.awscdk.pipelines.ShellStep;
+import software.amazon.awscdk.pipelines.StackDeployment;
+import software.amazon.awscdk.pipelines.StageDeployment;
 
 public class MyPipelineStack extends Stack {
     public MyPipelineStack(final Construct scope, final String id) {
@@ -18,19 +20,31 @@ public class MyPipelineStack extends Stack {
     public MyPipelineStack(final Construct scope, final String id, final StackProps props) {
         super(scope, id, props);
 
+        final CodePipelineSource source = CodePipelineSource.gitHub("hoangnhps11609/ci-cd-aws-pipeline", "main");
+
+        final ShellStep synth = ShellStep.Builder.create("Synth")
+                            .input(source)
+                            .commands(Arrays.asList("npm install -g aws-cdk", "cdk synth"))
+                            .build(); 
+
         final CodePipeline pipeline = CodePipeline.Builder.create(this, "pipeline")
             .pipelineName("MyPipeline")
-            .synth(ShellStep.Builder.create("Synth")
-                .input(CodePipelineSource.gitHub("hoangnhps11609/ci-cd-aws-pipeline", "main"))
-                .commands(Arrays.asList("npm install -g aws-cdk", "cdk synth"))
-                .build())
+            .synth(synth)
             .build();
+
+
+        final StageDeployment stage = 
+            pipeline.addStage(new MyPipelineAppStage(this, "test", StageProps.builder()
+                    .env(Environment.builder()
+                            .account("030245542280")
+                            .region("us-east-1")
+                            .build())
+                    .build()));
+
         
-        pipeline.addStage(new MyPipelineAppStage(this, "test", StageProps.builder()
-            .env(Environment.builder()
-                .account("030245542280")
-                .region("us-east-1")
-                .build())
-            .build()));
+        stage.addPost(ShellStep.Builder.create("validate")
+            .input(synth)
+            .commands(Arrays.asList("sh ./tests/validate.sh"))
+            .build());
     }
 }
